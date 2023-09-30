9d951d6128b67335a0b61b833fa2401a6cb54f5a
Spider <72209773+Cyberhero747@users.noreply.github.com>
2023-09-30 18:06:08 +0530

    Create largestin2.c

---
+++ largestin2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+    int a,b
+    printf("Enter the first number: ");
+    scanf("%d", a);
+    printf("Enter the second number: ");
+    scanf("%d", b);
+    if (a > b) {
+        printf("%d is greater than %d\n", a, b);
+    } else if (num2 > num1) {
+        printf("%d is greater than %d\n", b, a);
+    } else {
+        printf("Both numbers are equal: %d\n", a);
+    }
+}

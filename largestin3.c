6669e851bab23e5b29895674e468cd6a37dbb6a2
Nishant <72150854+Future423@users.noreply.github.com>
2023-10-01 06:47:48 +0530

    Update largestin3.c

--- largestin3.c
+++ largestin3.c
@@ -2,17 +2,17 @@
 void main() {
     int a,b,c;
     printf("Enter the first number: ");
-    scanf("%d", a);
+    scanf("%d", &a);
     printf("Enter the second number: ");
-    scanf("%d", b);
+    scanf("%d", &b);
     printf("Enter the third number: ");
-    scanf("%d", c);
+    scanf("%d", &c);
     if (a >= b && a >= c) {
         printf("%d is the greatest.\n", a);
     } else if (b >= a && b >= c) {
         printf("%d is the greatest.\n", b);
     } else {
-        printf("%d is the greatest.\n", b);
+        printf("%d is the greatest.\n", c);
     }
 
 }
